a7105d4b376af1915640281556201d175778615a
silverwedding <dan_e12@hotmail.com>
2017-03-22 23:45:58 +0000

    indentation

--- src/client/app/components/AddRecipe.js
+++ src/client/app/components/AddRecipe.js
@@ -13,19 +13,19 @@ const AddRecipe = ({ addRecipe }) => {
             aria-labelledby="myModalLabel">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
-                  <div className="modal-header">
-                    <button
-                        type="button"
-                        className="close"
-                        data-dismiss="modal"
-                        aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <h4 className="modal-title" id="myModalLabel">Modal title</h4>
-                  </div>
-                  <div className="modal-body">
-                      <AddRecipeFormContainer />
-                  </div>
+                    <div className="modal-header">
+                        <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <h4 className="modal-title" id="myModalLabel">Modal title</h4>
+                    </div>
+                    <div className="modal-body">
+                        <AddRecipeFormContainer />
+                    </div>
                 </div>
             </div>
         </div>
