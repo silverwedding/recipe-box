a8e90b9c5998dcbfe458f75b4623d44f70560e64
silverwedding <dan_e12@hotmail.com>
2017-03-22 22:45:06 +0000

    style webpack prod config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,5 @@
 var webpack = require("webpack");
 var path = require("path");
-const argv = require('argv');
 var CompressionPlugin = require("compression-webpack-plugin");
 
 var BUILD_DIR = path.resolve(__dirname, "src/client/public");
@@ -24,8 +23,8 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }, {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
+            test: /\.scss$/,
+            loaders: ["style", "css", "sass"]
         }]
     },
     resolve: {
@@ -39,33 +38,33 @@ module.exports = {
             "React": "react"
         }),
         new webpack.DefinePlugin({
-		  'process.env': {
-		    NODE_ENV: JSON.stringify('production')
-		  }
-		}),
-		new webpack.optimize.CommonsChunkPlugin('common.js'),
- 		new webpack.optimize.DedupePlugin(),
- 		new webpack.optimize.AggressiveMergingPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-	      mangle: true,
-	      compress: {
-	        warnings: false, // Suppress uglification warnings
-	        pure_getters: true,
-	        unsafe: true,
-	        unsafe_comps: true,
-	        screw_ie8: true
-	      },
-	      output: {
-	        comments: false,
-	      },
-	      exclude: [/\.min\.js$/gi] // skip pre-minified libs
-	    }),
-	    new CompressionPlugin({
-	      asset: "[path].gz[query]",
-	      algorithm: "gzip",
-	      test: /\.js$|\.css$|\.html$/,
-	      threshold: 10240,
-	      minRatio: 0
-	    })
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin("common.js"),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                warnings: false, // Suppress uglification warnings
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                screw_ie8: true
+            },
+            output: {
+                comments: false
+            },
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        }),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0
+        })
     ]
 };
