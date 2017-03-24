bff7e806ee3420ccd4bbee0f51162de2a8a9ef13
silverwedding <dan_e12@hotmail.com>
2017-03-24 23:18:10 +0000

    Add recipes

--- src/client/app/data/index.js
+++ src/client/app/data/index.js
@@ -1,30 +1,83 @@
 export const defaultRecipes = [
     {
         "id": 0,
-        "name": "Cheese & Ham Toastie",
+        "name": "Creamy Cajun Chicken Pasta",
         "ingredients": [
-            "fish", "cheese", "ham", "lemons"
+            "2 boneless skinless chicken breast halves, cut into thin strips",
+            "4 ounces linguine, cooked al dente",
+            "2 teaspoons cajun seasoning (your recipe, Cajun Seasoning Mix or store-bought)",
+            "2 tablespoons butter",
+            "1 thinly sliced green onion",
+            "1⁄2 cup heavy whipping cream",
+            "2 tablespoons chopped sun-dried tomatoes",
+            "1⁄4 teaspoon salt",
+            "1⁄4 teaspoon dried basil",
+            "1⁄8 teaspoon ground black pepper",
+            "1⁄8 teaspoon garlic powder"
         ],
-        "method": ["do something", "do something", "do something", "do something"]
+        "method": [
+            "Place chicken and Cajun seasoning in a bowl and toss to coat.",
+            "dIn a large skillet over medium heat, sauté chicken in butter or margarine until chicken is tender, about 5 to 7 minutes.",
+            "Reduce heat add green onion, heavy cream, tomatoes, basil, salt, garlic powder, black pepper and heat through.",
+            "Pour over hot linguine and toss with Parmesan cheese."
+        ]
     },
     {
         "id": 1,
-        "name": "Fish Surprise",
+        "name": "Reeses Squares",
         "ingredients": [
-            "orange", "apple", "cherry", "tomato"
+            "1 1⁄2 cups graham cracker crumbs",
+            "1 lb confectioners' sugar (3 to 3 1/2 cups)",
+            "1 1⁄2 cups peanut butter",
+            "1 cup butter, melted",
+            "1 (12 ounce) bag milk chocolate chips"
         ],
-        "method": ["do something", "do something", "do something", "do something"]
+        "method": [
+            "Combine graham crumbs, sugar and peanut butter and mix well.",
+            "Blend in melted butter until well combined.",
+            "Press mixture evenly into a 9 x 13 inch pan.",
+            "Melt chocolate chips in microwave or in double boiler.",
+            "Spread over peanut butter mixture.",
+            "Chill until just set and cut into bars (these are very hard to cut if the chocolate gets'rock hard'."
+        ]
     },
     {
         "id": 2,
-        "name": "Omelette Du Fromage",
-        "ingredients": ["orange", "apple", "cherry", "tomato"],
-        "method": ["do something", "do something", "do something", "do something"]
+        "name": "Bacon-Wrapped Jalapeno Poppers",
+        "ingredients": [
+            "25 fresh jalapeno peppers",
+            "14 -16 ounces cream cheese",
+            "2 cups shredded cheddar cheese",
+            "2 (16 ounce) packages bacon"
+        ],
+        "method": [
+            "Cut stems off of peppers and cut them all in half longways.",
+            "Remove seeds from peppers.",
+            "Fill each pepper with cream cheese and sprinkle cheddar cheese on top.",
+            "Wrap 1/2 slice of bacon around each pepper half.",
+            "Place on baking sheets and place in 450 degree oven for 10 to 15 minutes or until bacon is fully cooked.",
+            "Remove and serve when cooled."
+        ]
     },
     {
         "id": 3,
-        "name": "Spaghetti Marinara",
-        "ingredients": ["fish", "cheese", "ham", "lemons"],
-        "method": ["do something", "do something", "do something", "do something"]
+        "name": "Southern Sweet Iced Tea",
+        "ingredients": [
+            "6 regular tea bags",
+            "1⁄8 teaspoon baking soda",
+            "2 cups boiling water",
+            "1 1⁄2-2 cups sugar",
+            "6 cups cold water"
+        ],
+        "method": [
+            "In a large glass measuring cup, place the tea bags and add the baking soda.",
+            "Pour the boiling water over the tea bags. ",
+            "Cover and steep for 15 minutes.",
+            "Take out the tea bags and do not squeeze them.",
+            "Pour the tea mixture into a 2-quart pitcher; add the sugar.",
+            "Stir until the sugar is dissolved.",
+            "Add in the cold water.",
+            "Let cool; chill in the refrigerator and serve over ice."
+        ]
     }
 ];
